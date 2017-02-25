8802c84765a2b24c4f6b5c95d83b4c894925fdc2
AnaGermano6 <anasofiagermano@gmail.com>
2017-02-25 00:25:02 +0000

    final

--- src/man/ColorBombe.java
+++ src/man/ColorBombe.java
@@ -3,6 +3,7 @@ package man;
 import java.awt.Color;
 import java.util.Random;
 import robocode.*;
+import robocode.util.Utils;
 
 /**
  * 
@@ -12,107 +13,97 @@ import robocode.*;
 
 public class ColorBombe extends AdvancedRobot {
 
+	// Distância ao inimigo (relativamente à largura do campo)
+	// a que dispara com o máximo de potência
+	private final int MAX_FIRE_DIST = 30;
+	// Distância ao inimigo (relativamente à largura do campo)
+	// a que dispara com o mínimo de potência definida.
+	private final int MIN_FIRE_DIST = 70;
+	// Potência máxima de disparo
+	private final int MAX_FIRE = 3;
+	// Potência mínima de disparo
+	private final int MIN_FIRE = 2;
+	//Distância que o robô percorre de cada vez
+	private final int MOVE_DIST = 100;
+
 	private double lastTurnEnergy = 100;
 	private int moveDirection = 1;
-	private int graus = 360;
-	
-	/**
-	 * Este metodo faz com que o robot se mova
-	 * e tem as suas configurações iniciais
-	 */
-	
+
 	public void run() {
 		setRandomColors();
-		setTurnGunRight(graus);
+		setTurnRadarRight(Integer.MAX_VALUE);
 	}
-	
+
 	/**
-	 * Este metedo é usado quando um robot é detetado no radar.
-	 * Quando encontra um robot vira até ao inimigo para ficar frente a frente.
-	 * Verifica se o inimigo disparou usando a energia para escapar do tiro.
-	 * Apontar a arma, é feito depois do desvio, pois o robot pode ficar numa posicao diferente
-	 * E actualiza a energia
+	 * Quando encontra um robot vira até ao inimigo para ficar na horizontal e arma frente a
+	 * frente. Verifica se o inimigo disparou usando a energia para escapar do
+	 * tiro. Apontar a arma, é feito depois do desvio, pois o robot pode ficar
+	 * numa posicao diferente E actualiza a energia
 	 * 
-	 * @param e é um evento para quando um robot é detetado no radar
+	 * @param enemy é o evento ao encontrar um robô inimigo.
 	 */
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		//setRandomColors();
+	public void onScannedRobot(ScannedRobotEvent enemy) {
 
-		// 90 graus e angulo de 30
-		setTurnRight(e.getBearing()+ 90 - 30 * moveDirection);
-
-		
-		double energy = lastTurnEnergy - e.getEnergy();
+		//Verifica se o enimigo disparou
+		double energy = lastTurnEnergy - enemy.getEnergy();
 		if (energy <= 3 && energy > 0) {
-			// desviar de um tiro
+			//movimento zig zag
 			moveDirection = -moveDirection;
-			setAhead((e.getDistance() / 4 + 25) * moveDirection);
+			// 90 graus e angulo de 30
+			setTurnRight(enemy.getBearing() + 90 - 30 * moveDirection);
+			setAhead(MOVE_DIST * moveDirection);
+		} else{
+			//Se o inimigo não disparou, move-se em direção a ele 
+			setTurnRight(enemy.getBearing());
+			setAhead(MOVE_DIST);
 		}
 
-		moveDirection=-moveDirection;
-		setTurnGunRight(graus*moveDirection);
+		//Aponta para o inimigo e dispara
+		setTurnGunRight(aimGun(enemy));
+		fire(calcFireRate(enemy));
 
-		// calcula a intensidade e dispara
-		calcFireRate(e);
-
-		lastTurnEnergy = e.getEnergy();
+		lastTurnEnergy = enemy.getEnergy();
 	}
-	
 	/**
-	 * Este metodo calcula a intensidade do tiro de acordo 
-	 * com a distancia em que o robot inimigo se encontra
+	 * Calcula a rotação necessária para que a arma aponte para um robô alvo.
+	 * @param enemy é o robô ao qual se quer apontar a arma
+	 * @return movimento para a direita, em graus, necessário para apontar a arma ao inimigo
+	 */
+	
+	private double aimGun(ScannedRobotEvent enemy){
+		
+		double absEnemyAngle=enemy.getBearing()+getHeading();//ângulo entre o inimigo e o referêncial
+		double gunTurnDegrees = Utils.normalRelativeAngleDegrees(absEnemyAngle- getGunHeading())
+				+ enemy.getVelocity() * Math.sin(enemy.getHeading());//inclui ajuste devido ao movimento esperado do inimigo
+		
+		return gunTurnDegrees;
+	}
+
+	/**
+	 * Calcula a intensidade do tiro de acordo com a distancia em
+	 * que o robot inimigo se encontra, ou seja, quanto menor a
+	 * distancia, mais intensidade é do tiro
 	 * 
-	 * Quanto maior a distancia, menos intensidade do tiro
-	 * Quanto menor a distancia, mais intensidade é do tiro 
-	 * 
-	 * @param e é um evento para quando um robot é detetado no radar
+	 * @param enemy é o robô inimigo
+	 * @return potência do tiro
 	 */
 
-	private void calcFireRate(ScannedRobotEvent e) {
-		if (e.getDistance() > getBattleFieldWidth() * 0.8) // 80%
-			fire(0.1);
-		else if (e.getDistance() > getBattleFieldWidth() * 0.6) // 60%
-			fire(1);
-		else if (e.getDistance() > getBattleFieldWidth() * 0.4) // 40%
-			fire(2);
-		else if (e.getDistance() > getBattleFieldWidth() * 0.1) // 10%
-			fire(3);
-	}
-	
-	/**
-	 * Quando o robot bate na parede e poder sair
-	 */
-	public void onHitWall(HitWallEvent e) {
-		setTurnRight(100);
-	}
-	
-	
-	/**
-	 * Quando o robot bate noutro robot 
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-		setTurnRight(100);
+	private double calcFireRate(ScannedRobotEvent enemy) {
+
+		if (enemy.getDistance() < getBattleFieldWidth() * MAX_FIRE_DIST / 100)
+			return MAX_FIRE;
+		if (enemy.getDistance() > getBattleFieldWidth() * MIN_FIRE_DIST / 100)
+			return 0;
+		return MIN_FIRE;
 	}
 
 	
 	/**
-	 * Quando o robo leva um tiro
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		setTurnRight(100);
-	}
-	
-	
-	/**
-	 * Este metodo muda de cor aleatoriamente do robot
-	 * usando numero aleatorios entre 0 e 254
-	 * 
-	 * criando cores RGB 
-	 * 
+	 * Muda aleatóriamente as cores das componentes do robô
 	 */
 	private void setRandomColors() {
-		
+
 		Random randomGenerator = new Random();
 		int rValue = randomGenerator.nextInt(255);
 		int gValue = randomGenerator.nextInt(255);
@@ -123,6 +114,8 @@ public class ColorBombe extends AdvancedRobot {
 		setRadarColor(new Color((int) ((255 - rValue) / 2), (int) ((255 - gValue) / 2), (int) ((255 - bValue) / 2)));
 		setScanColor(new Color((int) ((255 - (rValue + 2)) / 2), (int) ((255 - (gValue + 2)) / 2),
 				(int) ((255 - (bValue + 2)) / 2)));
-		setBulletColor(new Color(rValue - 1, gValue - 1, bValue - 1));
+		setBulletColor(new Color(rValue - randomGenerator.nextInt(rValue), 
+				gValue - randomGenerator.nextInt(gValue), 
+				bValue - randomGenerator.nextInt(bValue)));
 	}
 }
